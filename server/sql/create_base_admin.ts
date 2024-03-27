d33857e369f581fda772f1d78f3ccfe90256dc17
Timeuh <timeuuh@gmail.com>
2024-03-27 21:32:09 +0100

    :sparkles: create base admin creation function

---
+++ server/sql/create_base_admin.ts
@@ -0,0 +1,29 @@
+import {Connection} from 'mysql2';
+import encryptPassword from '~/server/bcrypt/encrypt_password';
+
+/**
+ * Crée un utilisateur admin de base.
+ *
+ * @param db la connexion à la base de données
+ */
+const createBaseAdmin = async (db: Connection) => {
+  if (!process.env.ADMIN_USERNAME) {
+    throw new Error('Erreur : vous devez renseigner la variable d’environnement ADMIN_USERNAME dans le .env');
+  }
+
+  if (!process.env.ADMIN_PASSWORD) {
+    throw new Error('Erreur : vous devez renseigner la variable d’environnement ADMIN_PASSWORD dans le .env');
+  }
+
+  const hashedPassword = await encryptPassword(process.env.ADMIN_PASSWORD);
+
+  return db.execute(
+    `
+        INSERT INTO user (pseudo, password, role_id)
+        VALUES (?, ?, 1)
+    `,
+    [process.env.ADMIN_USERNAME, hashedPassword],
+  );
+};
+
+export default createBaseAdmin;
