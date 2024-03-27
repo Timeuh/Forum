ab244a2cf38bdb2a9a933de0950bd482f588bc2c
Timeuh <timeuuh@gmail.com>
2024-03-27 22:20:06 +0100

    :label: use correct return types

--- server/sql/create_base_admin.ts
+++ server/sql/create_base_admin.ts
@@ -1,12 +1,13 @@
-import {Connection, Query} from 'mysql2';
+import {FieldPacket, QueryResult} from 'mysql2';
 import encryptPassword from '~/server/bcrypt/encrypt_password';
+import mysql from 'mysql2/promise';
 
 /**
  * Crée un utilisateur admin de base.
  *
  * @param db la connexion à la base de données
  */
-const createBaseAdmin = async (db: Connection): Promise<Query> => {
+const createBaseAdmin = async (db: mysql.Connection): Promise<[QueryResult, FieldPacket[]]> => {
   if (!process.env.ADMIN_USERNAME) {
     throw new Error('Erreur : vous devez renseigner la variable d’environnement ADMIN_USERNAME dans le .env');
   }
