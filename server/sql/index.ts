81cfb724f2378fd1bffb6730ce8ce05c99e335ab
Timeuh <timeuuh@gmail.com>
2024-03-27 18:16:19 +0100

    :sparkles: create mysql connection fonction

---
+++ server/sql/index.ts
@@ -0,0 +1,19 @@
+import mysql from 'mysql2/promise';
+import bluebird from 'bluebird';
+
+/**
+ * Crée une connexion à la base de données
+ *
+ * @returns La connexion à la base de données dans une promesse
+ */
+const getConnection = (): Promise<mysql.Connection> => {
+  return mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    Promise: bluebird,
+  });
+};
+
+export default getConnection();
