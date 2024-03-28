472b90e36bdc0102afa2f588c4e9c895a9a925d9
Timeuh <timeuuh@gmail.com>
2024-03-28 09:57:28 +0100

    :recycle: return a proper response object when database existence check is a success

--- server/api/handleDatabase.get.ts
+++ server/api/handleDatabase.get.ts
@@ -1,9 +1,10 @@
 import getConnection from '~/server/sql';
 import checkDatabaseExistence from '~/server/sql/check_database_existence';
-import {ERROR_NO_SUCH_TABLE, HTTP_CREATED, HTTP_SERVER_ERROR} from '~/common/constants/api';
+import {ERROR_NO_SUCH_TABLE, HTTP_CREATED, HTTP_OK, HTTP_SERVER_ERROR} from '~/common/constants/api';
 import createTables from '~/server/sql/create_tables';
 import createBaseAdmin from '~/server/sql/create_base_admin';
 import {ApiError, ApiResponse} from '~/common/types/api';
+import {QueryResult} from 'mysql2';
 
 export default defineEventHandler(async (event) => {
   const db = await getConnection();
@@ -12,8 +13,9 @@ export default defineEventHandler(async (event) => {
     const [rows, fields] = await checkDatabaseExistence(db);
 
     return {
+      code: HTTP_OK,
       data: rows,
-    };
+    } as ApiResponse<QueryResult>;
   } catch (error: any) {
     if (error.errno !== ERROR_NO_SUCH_TABLE) {
       return {
