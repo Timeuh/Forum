948c7c4359b9b3c69502e0e4753f9bd6e6b82067
Timeuh <timeuuh@gmail.com>
2024-03-28 22:07:20 +0100

    :sparkles: add http error code 404 constant

--- common/constants/api.ts
+++ common/constants/api.ts
@@ -18,5 +18,8 @@ export const HTTP_CREATED = 201;
 // réponse http en cas de mauvaise requête de la part du client
 export const HTTP_BAD_REQUEST = 400;
 
+// réponse http en cas d'entité introuvable
+export const HTTP_NOT_FOUND = 404;
+
 // réponse http en cas d'erreur de l'application
 export const HTTP_SERVER_ERROR = 500;
