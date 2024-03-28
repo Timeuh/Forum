12ef5cde2a86ba69020fff7ed2c17f89e956cadf
Timeuh <timeuuh@gmail.com>
2024-03-28 21:38:56 +0100

    :sparkles: move to home page when clicking app name

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,13 +6,22 @@
   const goToLogin = () => {
     navigateTo('/login');
   };
+
+  const goToHome = () => {
+    navigateTo('/');
+  };
 </script>
 
 <template>
   <nav
     class="flex flex-row justify-around text-gray-300 items-center bg-slate-900 shadow-gray-300/50 shadow-sm fixed top-0 w-full h-14"
   >
-    <h2 class="text-xl font-bold text-purple-200">NuxtForum</h2>
+    <h2
+      class="text-xl font-bold text-purple-200 cursor-pointer hover:scale-105 transition duration-500"
+      @click="goToHome"
+    >
+      NuxtForum
+    </h2>
     <button
       @click="goToLogin"
       v-show="!isLogged"
