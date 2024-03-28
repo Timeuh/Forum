ba4621cfaf5af674b7c245a89fb571fa91ee82fb
Timeuh <timeuuh@gmail.com>
2024-03-28 09:33:56 +0100

    :sparkles: create types for api normal return and error return

---
+++ common/types/api.ts
@@ -0,0 +1,11 @@
+// objet réponse d'api
+export type ApiResponse = {
+  code: number;
+  message: string;
+};
+
+// objet erreur d'api
+export type ApiError = {
+  code: number;
+  message: string;
+};
