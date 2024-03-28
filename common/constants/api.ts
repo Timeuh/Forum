5436f9fdac5458529b06649c6a99b9959ad6a8ad
Timeuh <timeuuh@gmail.com>
2024-03-28 09:27:08 +0100

    :sparkles: add http status codes constants

--- common/constants/api.ts
+++ common/constants/api.ts
@@ -1,2 +1,22 @@
+/* -------------------------------------------------------------------------- */
+/*                            Erreurs Application                             */
+/* -------------------------------------------------------------------------- */
+
 // la table n'existe pas dans la base de données
 export const ERROR_NO_SUCH_TABLE = 1146;
+
+/* -------------------------------------------------------------------------- */
+/*                                 Codes HTTP                                 */
+/* -------------------------------------------------------------------------- */
+
+// réponse http en cas de succès
+export const HTTP_OK = 200;
+
+// réponse http en cas de création
+export const HTTP_CREATED = 201;
+
+// réponse http en cas de mauvaise requête de la part du client
+export const HTTP_BAD_REQUEST = 400;
+
+// réponse http en cas d'erreur de l'application
+export const HTTP_SERVER_ERROR = 500;
