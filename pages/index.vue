9470f94a8c94639dd435a4caad4c969babe3a647
Timeuh <timeuuh@gmail.com>
2024-03-28 19:08:52 +0100

    :sparkles: add page head title and change app colors

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+  useHead({
+    title: 'Nuxt Forum',
+  });
+
   onMounted(async () => {
     const databaseCreation = await $fetch('/api/handleDatabase');
     console.log(databaseCreation);
@@ -6,7 +10,7 @@
 </script>
 
 <template>
-  <section id="home" class="flex w-full h-screen">
+  <section id="home" class="flex w-full h-screen bg-slate-900 text-gray-300">
     <h1 class="m-auto text-4xl">Welcome !</h1>
   </section>
 </template>
