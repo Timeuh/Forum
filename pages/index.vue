7b10847e08ad0627e5efa596b6788ce43d4bb01b
Timeuh <timeuuh@gmail.com>
2024-03-29 09:44:21 +0100

    :bug: handle case of docker compose not being up

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,22 @@
     title: 'Nuxt Forum',
   });
 
+  const appError = ref<string>('');
+
   onMounted(async () => {
-    const databaseCreation = await $fetch('/api/handleDatabase');
-    console.log(databaseCreation);
+    try {
+      const databaseCreation = await $fetch('/api/handleDatabase');
+      console.log(databaseCreation);
+    } catch (error: any) {
+      appError.value = error.data.error;
+      console.log(error.value);
+    }
   });
 </script>
 
 <template>
-  <section id="home" class="flex w-full h-screen bg-slate-900 text-gray-300">
+  <section id="home" class="flex flex-col w-full h-screen bg-slate-900 text-gray-300">
+    <h1 v-show="appError !== ''" class="m-auto text-4xl text-red-300">{{ appError }}</h1>
     <h1 class="m-auto text-4xl">Welcome !</h1>
   </section>
 </template>
--- server/api/handleDatabase.get.ts
+++ server/api/handleDatabase.get.ts
@@ -7,40 +7,48 @@ import {ApiError, ApiResponse} from '~/common/types/api';
 import {QueryResult} from 'mysql2';
 
 export default defineEventHandler(async (event) => {
-  const db = await getConnection();
-
   try {
-    const [rows, fields] = await checkDatabaseExistence(db);
-
-    setResponseStatus(event, HTTP_OK);
-    return {
-      code: HTTP_OK,
-      data: rows,
-    } as ApiResponse<QueryResult>;
-  } catch (error: any) {
-    if (error.errno !== ERROR_NO_SUCH_TABLE) {
-      setResponseStatus(event, HTTP_SERVER_ERROR);
-      return {
-        code: HTTP_SERVER_ERROR,
-        error: error.message,
-      } as ApiError;
-    }
+    const db = await getConnection();
 
     try {
-      await createTables(db);
-      await createBaseAdmin(db);
+      const [rows, fields] = await checkDatabaseExistence(db);
 
-      setResponseStatus(event, HTTP_CREATED);
+      setResponseStatus(event, HTTP_OK);
       return {
-        code: HTTP_CREATED,
-        message: 'Base de donnée créée, avec un admin par défaut.',
-      } as ApiResponse;
+        code: HTTP_OK,
+        data: rows,
+      } as ApiResponse<QueryResult>;
     } catch (error: any) {
-      setResponseStatus(event, HTTP_SERVER_ERROR);
-      return {
-        code: HTTP_SERVER_ERROR,
-        error: error.message,
-      } as ApiError;
+      if (error.errno !== ERROR_NO_SUCH_TABLE) {
+        setResponseStatus(event, HTTP_SERVER_ERROR);
+        return {
+          code: HTTP_SERVER_ERROR,
+          error: error.message,
+        } as ApiError;
+      }
+
+      try {
+        await createTables(db);
+        await createBaseAdmin(db);
+
+        setResponseStatus(event, HTTP_CREATED);
+        return {
+          code: HTTP_CREATED,
+          message: 'Base de donnée créée, avec un admin par défaut.',
+        } as ApiResponse;
+      } catch (error: any) {
+        setResponseStatus(event, HTTP_SERVER_ERROR);
+        return {
+          code: HTTP_SERVER_ERROR,
+          error: error.message,
+        } as ApiError;
+      }
     }
+  } catch (error: any) {
+    setResponseStatus(event, HTTP_SERVER_ERROR);
+    return {
+      code: HTTP_SERVER_ERROR,
+      error: 'Vous devez démarrer le docker-compose pour utiliser la base de données.',
+    } as ApiError;
   }
 });
