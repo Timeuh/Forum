3cd4d67dffb5e13e400477a7b2de7f07b5cd5280
Timeuh <timeuuh@gmail.com>
2024-03-29 09:49:10 +0100

    :sparkles: display message when database is initialized

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,17 @@
   });
 
   const appError = ref<string>('');
+  const creationStatus = ref<string>('');
 
   onMounted(async () => {
     try {
       const databaseCreation = await $fetch('/api/handleDatabase');
-      console.log(databaseCreation);
+
+      if (databaseCreation.code === 201 && 'message' in databaseCreation) {
+        creationStatus.value = databaseCreation.message;
+      }
     } catch (error: any) {
       appError.value = error.data.error;
-      console.log(error.value);
     }
   });
 </script>
@@ -19,6 +22,7 @@
 <template>
   <section id="home" class="flex flex-col w-full h-screen bg-slate-900 text-gray-300">
     <h1 v-show="appError !== ''" class="m-auto text-4xl text-red-300">{{ appError }}</h1>
+    <h1 v-show="creationStatus !== ''" class="m-auto text-4xl text-green-300">{{ creationStatus }}</h1>
     <h1 class="m-auto text-4xl">Welcome !</h1>
   </section>
 </template>
