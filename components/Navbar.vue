43aaed480ba9d0bfdd4fdef19009b346058ab38d
Timeuh <timeuuh@gmail.com>
2024-03-29 15:00:31 +0100

    :sparkles: redirect to profile page when clicking user icon

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,6 +16,10 @@
     userStore.logout();
     navigateTo('/');
   };
+
+  const goToProfile = () => {
+    navigateTo('/profile');
+  };
 </script>
 
 <template>
@@ -50,6 +54,7 @@
       </div>
       <div v-show="isLogged">
         <svg
+          @click="goToProfile"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
           height="24"
@@ -59,6 +64,7 @@
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
+          class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer"
         >
           <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
           <circle cx="12" cy="7" r="4" />
