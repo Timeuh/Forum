8dc63ab9fadb287dcdf2b34f83fe787a96b969ec
Timeuh <timeuuh@gmail.com>
2024-03-29 14:25:11 +0100

    :sparkles: handle errors + login redirect

--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
+  import {HTTP_OK} from '~/common/constants/api';
+
   const pseudo = ref<string>('');
   const password = ref<string>('');
+  const error = ref<string>('');
 
-  const userStore = useUserStore();
-  const {user} = storeToRefs(userStore);
-  const {login} = userStore;
+  const {login} = useUserStore();
 
   const handleSubmit = async (event: Event) => {
     event.preventDefault();
     const loginResponse = await login(pseudo.value, password.value);
-    console.log(loginResponse);
-    console.log(user.value);
+
+    if (loginResponse.code !== HTTP_OK && 'error' in loginResponse) {
+      error.value = loginResponse.error;
+      return;
+    }
+
+    navigateTo('/');
   };
 </script>
 
@@ -18,6 +24,7 @@
   <section class="flex h-screen w-full bg-slate-900 text-gray-200">
     <form @submit="handleSubmit" class="m-auto">
       <h1>Connexion</h1>
+      <h2 v-show="error !== ''">{{ error }}</h2>
       <label for="pseudo">Pseudo</label>
       <input type="text" name="pseudo" placeholder="johndoe" v-model="pseudo" />
       <label for="password">Mot de passe</label>
