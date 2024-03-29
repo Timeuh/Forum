4aab51c35381566457f02fd96b74105edc0b435d
Timeuh <timeuuh@gmail.com>
2024-03-29 15:00:10 +0100

    :sparkles: create profile vue (WIP)

---
+++ pages/profile.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const {user} = useUserStore();
+</script>
+
+<template>
+  <section class="bg-slate-900 flex w-full h-screen text-gray-200">
+    <h1 class="m-auto">{{ user.pseudo }}</h1>
+  </section>
+</template>
