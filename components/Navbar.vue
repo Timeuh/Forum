9a1ee7944d6faabc7a47bd60d6e9a28e0e487e92
Timeuh <timeuuh@gmail.com>
2024-03-29 14:57:42 +0100

    :recycle: use icons instead of buttons for login/logout + display pseudo when logged in

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -28,20 +28,62 @@
     >
       NuxtForum
     </h2>
-    <h2>{{ user.pseudo }}</h2>
-    <button
-      @click="goToLogin"
-      v-show="!isLogged"
-      class="p-2 px-4 bg-purple-200 rounded-lg text-slate-900 text-lg hover:bg-purple-300 transition duration-500"
-    >
-      Connexion
-    </button>
-    <button
-      @click="handleLogout"
-      v-show="isLogged"
-      class="p-2 px-4 bg-purple-200 rounded-lg text-slate-900 text-lg hover:bg-purple-300 transition duration-500"
-    >
-      Déconnexion
-    </button>
+    <div class="p-2 px-4 bg-purple-200 rounded-lg text-slate-900 text-lg flex flex-row items-center">
+      <div v-show="!isLogged">
+        <svg
+          @click="goToLogin"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer"
+        >
+          <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+          <polyline points="10 17 15 12 10 7" />
+          <line x1="15" x2="3" y1="12" y2="12" />
+        </svg>
+      </div>
+      <div v-show="isLogged">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+          <circle cx="12" cy="7" r="4" />
+        </svg>
+      </div>
+      <h2 v-show="isLogged" class="px-4">{{ user.pseudo }}</h2>
+      <div v-show="isLogged">
+        <svg
+          @click="handleLogout"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer"
+        >
+          <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+          <polyline points="16 17 21 12 16 7" />
+          <line x1="21" x2="9" y1="12" y2="12" />
+        </svg>
+      </div>
+    </div>
   </nav>
 </template>
