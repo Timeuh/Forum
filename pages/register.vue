1ff3c21a8e96187580724be301b348e50e227230
Timeuh <timeuuh@gmail.com>
2024-03-29 16:52:07 +0100

    :sparkles: create register page (WIP)

---
+++ pages/register.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="flex h-screen w-full bg-slate-900 text-gray-200">
+    <h1 class="m-auto">S'inscrire</h1>
+  </section>
+</template>
