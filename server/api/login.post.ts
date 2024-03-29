77cd6764d314f44a75866a4f41556d639cd03913
Timeuh <timeuuh@gmail.com>
2024-03-29 14:13:00 +0100

    :sparkles: check password correspondance and return user if it's ok

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -1,8 +1,9 @@
 import getConnection from '~/server/sql';
 import mysql, {RowDataPacket} from 'mysql2/promise';
-import {HTTP_NOT_FOUND, HTTP_SERVER_ERROR} from '~/common/constants/api';
+import {HTTP_BAD_REQUEST, HTTP_NOT_FOUND, HTTP_SERVER_ERROR} from '~/common/constants/api';
 import {ApiError, ApiResponse} from '~/common/types/api';
 import {User} from '~/common/types/app';
+import validatePassword from '~/server/bcrypt/validate_password';
 
 function isRowDataPacket(row: any): row is RowDataPacket[] {
   return Array.isArray(row) && row.length > 0 && typeof row[0] === 'object';
@@ -13,7 +14,10 @@ export default defineEventHandler(async (event): Promise<ApiError | ApiResponse<
     const db: mysql.Connection = await getConnection();
     const body = await readBody(event);
 
-    const [assumedUser] = await db.execute('SELECT * FROM user WHERE pseudo = ?', [body.pseudo]);
+    const [assumedUser] = await db.execute(
+      'SELECT * FROM user inner join role on user.role_id = role.id WHERE pseudo = ?',
+      [body.pseudo],
+    );
 
     if (!isRowDataPacket(assumedUser)) {
       setResponseStatus(event, HTTP_NOT_FOUND);
@@ -23,9 +27,21 @@ export default defineEventHandler(async (event): Promise<ApiError | ApiResponse<
       } as ApiError;
     }
 
+    const isPasswordValid = await validatePassword(body.password, assumedUser[0].password);
+    if (!isPasswordValid) {
+      setResponseStatus(event, HTTP_BAD_REQUEST);
+      return {
+        code: HTTP_BAD_REQUEST,
+        error: 'Mot de passe incorrect',
+      } as ApiError;
+    }
+
     return {
       code: 200,
-      data: assumedUser[0] as User,
+      data: {
+        pseudo: assumedUser[0].pseudo,
+        role: assumedUser[0].name,
+      } as User,
     } as ApiResponse<User>;
   } catch (error: any) {
     setResponseStatus(event, HTTP_SERVER_ERROR);
