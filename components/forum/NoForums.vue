745c01a900f9be5743e3aa5adf1231796707a5ad
Timeuh <timeuuh@gmail.com>
2024-04-01 14:11:19 +0200

    :recycle: move no forum message display into dedicated component

---
+++ components/forum/NoForums.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import {useForumStore} from '~/stores/forum.store';
+
+  const {hasForums} = useForumStore();
+</script>
+
+<template>
+  <div v-show="!hasForums" class="flex flex-row space-x-2 items-center">
+    <h2 class="text-2xl">Il n'y a aucun forum pour le moment,</h2>
+    <button class="text-2xl text-purple-300 font-bold">créez-en un !</button>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import {useForumStore} from '~/stores/forum.store';
+  import NoForums from '~/components/forum/NoForums.vue';
 
   useHead({
     title: 'Nuxt Forum',
@@ -7,7 +8,7 @@
 
   const appError = ref<string>('');
   const creationStatus = ref<string>('');
-  const {chargeForums, hasForums} = useForumStore();
+  const {chargeForums} = useForumStore();
 
   onMounted(async () => {
     try {
@@ -31,10 +32,7 @@
   >
     <h1 class="text-4xl">Bienvenue sur Nuxt Forum !</h1>
     <h1 v-show="appError !== ''" class="text-4xl text-red-300">{{ appError }}</h1>
-    <div v-show="!hasForums" class="flex flex-row space-x-2 items-center">
-      <h2 class="text-2xl">Il n'y a aucun forum pour le moment,</h2>
-      <button class="text-2xl text-purple-300 font-bold">créez-en un !</button>
-    </div>
+    <NoForums />
     <h1 v-show="creationStatus !== ''" class="text-4xl text-green-300">{{ creationStatus }}</h1>
   </section>
 </template>
