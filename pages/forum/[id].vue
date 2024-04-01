02b0030f8c2d27a1a79178ddc3d035ff72373e35
Timeuh <timeuuh@gmail.com>
2024-04-01 21:22:26 +0200

    :sparkles: add subject creation form display

--- pages/forum/[id].vue
+++ pages/forum/[id].vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
+  import formatDate from '../../common/functions/formatDate';
+  import {useSubjectStore} from '~/stores/subject.store';
+  import SubjectForm from '~/components/subject/SubjectForm.vue';
+
   const route = useRoute();
 
   const forumStore = useForumStore();
   const {currentForum} = storeToRefs(forumStore);
   const {fetchForum} = forumStore;
 
-  onMounted(async () => {
-    await fetchForum(route.params.id as string);
-  });
+  const {toggleCreationForm} = useSubjectStore();
+
+  await fetchForum(route.params.id as string);
 </script>
 
 <template>
-  <section class="h-screen w-full flex flex-col items-center justify-center space-y-8 bg-slate-900 text-gray-200">
+  <section
+    class="min-h-screen h-fit py-8 w-full flex flex-col items-center justify-center space-y-8 bg-slate-900 text-gray-200"
+  >
     <NuxtLink
       href="/"
       class="align-self-start ps-12 flex flex-row items-center justify-center space-x-4 hover:scale-110 transition duration-300 ease-in-out"
@@ -36,5 +42,24 @@
     >
       <h1 class="text-4xl font-bold">Forum {{ currentForum.forum.name }}</h1>
     </div>
+    <h2 class="align-self-start ps-[18vw]">Créé le {{ formatDate(currentForum.forum.created_at) }}</h2>
+    <NuxtLink
+      :href="`/subject/${subject.id}`"
+      v-for="subject in currentForum.subjects"
+      class="hover:scale-110 transition duration-300 ease-in-out shadow-md cursor-pointer shadow-gray-200 bg-gray-200 rounded-lg space-x-20 p-4 h-fit text-slate-800 flex flex-row w-2/3 justify-around items-center"
+    >
+      <div>
+        <h1 class="text-2xl font-bold">{{ subject.name }}</h1>
+        <h2 class="text-md">Mis à jour le {{ formatDate(subject.created_at) }}</h2>
+      </div>
+    </NuxtLink>
+    <div
+      v-show="currentForum.subjects.length === 0"
+      class="space-x-2 shadow-md justify-center shadow-gray-200 bg-gray-200 rounded-lg p-4 h-fit text-slate-800 flex flex-row w-2/3 items-center"
+    >
+      <h1 class="text-2xl">Il n'y a aucun sujet dans ce forum,</h1>
+      <button @click="toggleCreationForm" class="text-2xl text-purple-900 font-bold">créez-en un !</button>
+    </div>
+    <SubjectForm />
   </section>
 </template>
