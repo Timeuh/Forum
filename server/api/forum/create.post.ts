c0824e951ae1b210507c963e91c3de2e0bfd023e
Timeuh <timeuuh@gmail.com>
2024-04-01 17:49:51 +0200

    :sparkles: parse forum name to prevent XSS + make sure same forum doesn't exist

--- server/api/forum/create.post.ts
+++ server/api/forum/create.post.ts
@@ -1,14 +1,28 @@
 import getConnection from '~/server/sql';
 import mysql from 'mysql2/promise';
-import {HTTP_CREATED, HTTP_SERVER_ERROR} from '~/common/constants/api';
+import {HTTP_BAD_REQUEST, HTTP_CREATED, HTTP_SERVER_ERROR} from '~/common/constants/api';
 import {ApiError, ApiResponse} from '~/common/types/api';
+import validator from 'validator';
+import isRowDataPacket from '~/server/functions/isRowDataPacket';
 
 export default defineEventHandler(async (event): Promise<ApiError | ApiResponse> => {
   try {
     const db: mysql.Connection = await getConnection();
     const {name} = await readBody(event);
 
-    await db.query('INSERT INTO forum (name, created_at) VALUES (?, NOW())', [name]);
+    const parsedName = validator.escape(name);
+
+    const [existenceCheck] = await db.query('SELECT id FROM forum WHERE name = ?', [parsedName]);
+
+    if (isRowDataPacket(existenceCheck)) {
+      setResponseStatus(event, HTTP_BAD_REQUEST);
+      return {
+        code: HTTP_BAD_REQUEST,
+        error: 'Ce forum existe déjà',
+      } as ApiError;
+    }
+
+    await db.query('INSERT INTO forum (name, created_at) VALUES (?, NOW())', [parsedName]);
 
     setResponseStatus(event, HTTP_CREATED);
     return {
