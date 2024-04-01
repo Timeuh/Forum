d2d2434a0b4fbf1f242a3f37373ef0093d34e921
Timeuh <timeuuh@gmail.com>
2024-04-01 21:29:45 +0200

    :sparkles: show subject creation form only if user is logged

--- components/subject/SubjectForm.vue
+++ components/subject/SubjectForm.vue
@@ -5,6 +5,9 @@
   const {displayCreationForm} = storeToRefs(subjectStore);
   const {toggleCreationForm} = subjectStore;
 
+  const userStore = useUserStore();
+  const {isLogged} = storeToRefs(userStore);
+
   const name = ref<string>('');
   const content = ref<string>('');
   const error = ref<string>('');
@@ -27,33 +30,36 @@
     >
       X
     </div>
-    <h2 class="text-2xl">Créer un nouveau sujet</h2>
-    <h2 class="text-xl text-red-500">{{ error }}</h2>
-    <div class="relative w-2/3">
-      <label for="name" class="absolute -top-3 left-4 bg-gray-200 px-1">Nom</label>
-      <input
-        type="text"
-        name="name"
-        placeholder="Mon sujet"
-        v-model="name"
-        class="border border-slate-900 rounded-md p-2 pt-3 focus:outline-slate-800 w-full"
-        required
-      />
+    <h2 v-show="!isLogged" class="text-3xl text-red-500 font-bold">Vous devez être connecté pour créer un sujet</h2>
+    <div v-show="isLogged" class="space-y-12 h-fit flex flex-col w-full justify-center items-center">
+      <h2 class="text-2xl">Créer un nouveau sujet</h2>
+      <h2 class="text-xl text-red-500">{{ error }}</h2>
+      <div class="relative w-2/3">
+        <label for="name" class="absolute -top-3 left-4 bg-gray-200 px-1">Nom</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Mon sujet"
+          v-model="name"
+          class="border border-slate-900 rounded-md p-2 pt-3 focus:outline-slate-800 w-full"
+          required
+        />
+      </div>
+      <div class="relative w-2/3">
+        <label for="content" class="absolute -top-3 left-4 bg-gray-200 px-1">Contenu</label>
+        <textarea
+          name="content"
+          placeholder="Mon sujet est formidable"
+          v-model="content"
+          class="border border-slate-900 rounded-md p-2 pt-3 focus:outline-slate-800 resize-none w-full min-h-[20vh]"
+          required
+        />
+      </div>
+      <button
+        class="p-4 px-6 text-xl font-bold bg-purple-200 rounded-lg hover:shadow-lg hover:shadow-purple-400 transition duration-300 ease-in-out delay-100"
+      >
+        Créer
+      </button>
     </div>
-    <div class="relative w-2/3">
-      <label for="content" class="absolute -top-3 left-4 bg-gray-200 px-1">Contenu</label>
-      <textarea
-        name="content"
-        placeholder="Mon sujet est formidable"
-        v-model="content"
-        class="border border-slate-900 rounded-md p-2 pt-3 focus:outline-slate-800 resize-none w-full min-h-[20vh]"
-        required
-      />
-    </div>
-    <button
-      class="p-4 px-6 text-xl font-bold bg-purple-200 rounded-lg hover:shadow-lg hover:shadow-purple-400 transition duration-300 ease-in-out delay-100"
-    >
-      Créer
-    </button>
   </form>
 </template>
