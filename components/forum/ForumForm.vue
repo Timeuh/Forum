b70b604c0ca724d30500b7bdbc9db588bf29fc6c
Timeuh <timeuuh@gmail.com>
2024-04-01 14:49:42 +0200

    :sparkles: create forum form (WIP)

---
+++ components/forum/ForumForm.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import {useForumStore} from '~/stores/forum.store';
+
+  const forumStore = useForumStore();
+  const {displayCreationForm} = storeToRefs(forumStore);
+</script>
+
+<template>
+  <div v-show="displayCreationForm">
+    <h2>Formulaire de création</h2>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import {useForumStore} from '~/stores/forum.store';
   import NoForums from '~/components/forum/NoForums.vue';
+  import ForumForm from '~/components/forum/ForumForm.vue';
 
   useHead({
     title: 'Nuxt Forum',
@@ -8,6 +9,7 @@
 
   const appError = ref<string>('');
   const creationStatus = ref<string>('');
+
   const {chargeForums} = useForumStore();
 
   onMounted(async () => {
@@ -31,6 +33,7 @@
     class="flex flex-col w-full h-screen bg-slate-900 text-gray-300 justify-center items-center space-y-12"
   >
     <NoForums />
+    <ForumForm />
     <h1 v-show="appError !== ''" class="text-4xl text-red-300">{{ appError }}</h1>
     <h1 v-show="creationStatus !== ''" class="text-4xl text-green-300">{{ creationStatus }}</h1>
   </section>
