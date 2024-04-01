caea17b8d1c33786d77ceb2d1363032127c9f811
Timeuh <timeuuh@gmail.com>
2024-04-01 14:05:42 +0200

    :wrench: remove devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify';
 
 export default defineNuxtConfig({
-  devtools: {enabled: true},
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/eslint-module',
