97676cf996784e7ac64a90c0992fe8f35323a08a
Timeuh <timeuuh@gmail.com>
2024-04-01 20:37:33 +0200

    :label: add subject and fullforum types

--- common/types/app.ts
+++ common/types/app.ts
@@ -14,3 +14,18 @@ export type Forum = {
   created_at: string;
   subject_count: number;
 };
+
+// les informations d'un sujet
+export type Subject = {
+  id: number;
+  name: string;
+  forum_id: number;
+  created_at: string;
+  original_message_id: number;
+};
+
+// un forum avec ses sujets
+export type FullForum = {
+  forum: Forum;
+  subjects: Subject[];
+};
