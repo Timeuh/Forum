1fb916e6fb85eb7f43929e724cecf8aca2e02b2c
Timeuh <timeuuh@gmail.com>
2024-04-01 10:57:50 +0200

    :sparkles: handle validations and account creation

--- server/api/register.ts
+++ server/api/register.ts
@@ -1,12 +1,37 @@
 import getConnection from '~/server/sql';
 import mysql from 'mysql2/promise';
-import {HTTP_CREATED, HTTP_SERVER_ERROR} from '~/common/constants/api';
+import {HTTP_BAD_REQUEST, HTTP_CREATED, HTTP_SERVER_ERROR} from '~/common/constants/api';
 import {ApiError, ApiResponse} from '~/common/types/api';
+import validator from 'validator';
+import isRowDataPacket from '~/server/functions/isRowDataPacket';
+import encryptPassword from '~/server/bcrypt/encrypt_password';
 
 export default defineEventHandler(async (event): Promise<ApiError | ApiResponse> => {
   try {
     const db: mysql.Connection = await getConnection();
     const {pseudo, password, repeatedPassword} = await readBody(event);
+
+    const parsedPseudo = validator.escape(pseudo);
+
+    const [existingUser] = await db.execute('SELECT id FROM user WHERE pseudo = ?', [parsedPseudo]);
+    if (isRowDataPacket(existingUser)) {
+      setResponseStatus(event, HTTP_BAD_REQUEST);
+      return {
+        code: HTTP_BAD_REQUEST,
+        error: 'Utilisateur déjà existant',
+      } as ApiError;
+    }
+
+    if (!validator.equals(password, repeatedPassword)) {
+      setResponseStatus(event, HTTP_BAD_REQUEST);
+      return {
+        code: HTTP_BAD_REQUEST,
+        error: 'Les mots de passe ne correspondent pas',
+      } as ApiError;
+    }
+
+    const encodedPassword = await encryptPassword(password);
+    await db.execute('INSERT INTO user (pseudo, password, role_id) VALUES (?, ?, 2)', [parsedPseudo, encodedPassword]);
 
     setResponseStatus(event, HTTP_CREATED);
     return {
