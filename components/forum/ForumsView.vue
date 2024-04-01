05c41fc112cf90a30f9d1d3a8f90f8eedffc97dd
Timeuh <timeuuh@gmail.com>
2024-04-01 16:55:08 +0200

    :sparkles: add forum creation form toggle

--- components/forum/ForumsView.vue
+++ components/forum/ForumsView.vue
@@ -3,7 +3,7 @@
 
   const forumStore = useForumStore();
   const {forums, hasForums} = storeToRefs(forumStore);
-  const {chargeForums} = forumStore;
+  const {chargeForums, toggleCreationForm} = forumStore;
 
   onMounted(async () => {
     await chargeForums();
@@ -11,7 +11,11 @@
 </script>
 
 <template>
-  <section v-show="hasForums" class="w-full h-full flex flex-col items-center justify-center space-y-8 text-slate-900">
+  <section v-show="hasForums" class="w-full h-fit flex flex-col items-center justify-center space-y-8 text-slate-900">
+    <div class="flex flex-row space-x-2 items-center">
+      <h2 class="text-2xl text-gray-200">Si vous ne trouvez pas votre forum,</h2>
+      <button class="text-2xl text-purple-300 font-bold" @click="toggleCreationForm">créez-le !</button>
+    </div>
     <div
       v-for="forum in forums"
       class="shadow-md shadow-gray-200 bg-gray-200 rounded-lg space-x-20 p-4 h-fit text-slate-800 flex flex-row w-2/3 justify-center items-center"
