fb8fd49d12485e85072902016d7bf1a0d1f47d5c
Timeuh <timeuuh@gmail.com>
2024-04-01 11:15:32 +0200

    :sparkles: add button to go create a new admin

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
   import {useUserStore} from '~/stores/user.store';
 
   const userStore = useUserStore();
-  const {isLogged, user} = storeToRefs(userStore);
+  const {isLogged, isAdmin, user} = storeToRefs(userStore);
 
   const goToLogin = () => {
     navigateTo('/login');
@@ -19,6 +19,10 @@
 
   const goToProfile = () => {
     navigateTo('/profile');
+  };
+
+  const goToAdminRegister = () => {
+    navigateTo('/admin-register');
   };
 </script>
 
@@ -66,8 +70,29 @@
           stroke-linejoin="round"
           class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer"
         >
-          <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-          <circle cx="12" cy="7" r="4" />
+          <rect width="18" height="18" x="3" y="3" rx="2" />
+          <circle cx="12" cy="10" r="3" />
+          <path d="M7 21v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2" />
+        </svg>
+      </div>
+      <div v-show="isAdmin" class="px-2">
+        <svg
+          @click="goToAdminRegister"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="hover:scale-110 transition duration-300 ease-in-out cursor-pointer"
+        >
+          <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+          <circle cx="9" cy="7" r="4" />
+          <line x1="19" x2="19" y1="8" y2="14" />
+          <line x1="22" x2="16" y1="11" y2="11" />
         </svg>
       </div>
       <h2 v-show="isLogged" class="px-4">{{ user.pseudo }}</h2>
