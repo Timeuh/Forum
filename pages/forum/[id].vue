1eb8e500b391a9d07f342f4a1f113b62030f4d78
Timeuh <timeuuh@gmail.com>
2024-04-01 18:47:54 +0200

    :sparkles: create forum page (WIP)

---
+++ pages/forum/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const route = useRoute();
+</script>
+
+<template>
+  <section class="h-screen w-full flex flex-col items-center justify-center space-y-8 bg-slate-900 text-gray-200">
+    <h1>forum {{ route.params.id }}</h1>
+  </section>
+</template>
