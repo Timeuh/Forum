8e83a708c299c80bd5a70bfeedd7fdf86adc1dc3
Timeuh <timeuuh@gmail.com>
2024-04-01 20:42:11 +0200

    :sparkles: create api route to get a forum and its subjects

---
+++ server/api/forum/get.post.ts
@@ -0,0 +1,41 @@
+import getConnection from '~/server/sql';
+import mysql from 'mysql2/promise';
+import {HTTP_OK, HTTP_SERVER_ERROR} from '~/common/constants/api';
+import {ApiError, ApiResponse} from '~/common/types/api';
+import isRowDataPacket from '~/server/functions/isRowDataPacket';
+
+export default defineEventHandler(async (event): Promise<ApiError | ApiResponse> => {
+  try {
+    const db: mysql.Connection = await getConnection();
+    const {id} = await readBody(event);
+
+    const [forum] = await db.query('SELECT * FROM forum WHERE id = ?;', [id]);
+    const [subjects] = await db.query('SELECT * FROM subject WHERE forum_id = ?;', [id]);
+
+    console.log(forum);
+    console.log(subjects);
+
+    if (!isRowDataPacket(forum)) {
+      setResponseStatus(event, HTTP_SERVER_ERROR);
+      return {
+        code: HTTP_SERVER_ERROR,
+        error: 'Forum non trouvé',
+      } as ApiError;
+    }
+
+    setResponseStatus(event, HTTP_OK);
+    return {
+      code: HTTP_OK,
+      data: {
+        forum: forum[0],
+        subjects: subjects,
+      },
+    } as ApiResponse;
+  } catch (error: any) {
+    setResponseStatus(event, HTTP_SERVER_ERROR);
+    return {
+      code: HTTP_SERVER_ERROR,
+      error: error.message,
+    } as ApiError;
+  }
+});
