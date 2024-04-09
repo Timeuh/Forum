c4f3376d442de1c984ba45cb5d422b39dc609718
Timeuh <timeuuh@gmail.com>
2024-04-09 11:02:54 +0200

    :sparkles: add function to create a new message

--- stores/message.store.ts
+++ stores/message.store.ts
@@ -1,3 +1,5 @@
+import type {ApiError} from '~/common/types/api';
+
 export const useMessageStore = defineStore('message', {
   state: () => {
     return {
@@ -8,6 +10,24 @@ export const useMessageStore = defineStore('message', {
   actions: {
     toggleAnswerForm(): void {
       this.displayAnswerForm = !this.displayAnswerForm;
+    },
+    async createMessage(content: string, subjectId: number, userId: number): Promise<void | ApiError> {
+      try {
+        return await $fetch('/api/message/create', {
+          method: 'POST',
+          body: JSON.stringify({
+            content,
+            subjectId,
+            userId,
+          }),
+        });
+      } catch (error: any) {
+        if ('data' in error) {
+          return error.data;
+        }
+
+        return error;
+      }
     },
   },
 });
